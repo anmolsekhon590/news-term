cd0ca4f9235650ce378e51c800c9d7cbd6c825ed
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-03-05 15:38:07 -0500

    now shows top 10 news

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from bs4 import BeautifulSoup
 response = requests.get("https://www.cbc.ca/news")
 content = BeautifulSoup(response.content, "html.parser")
 
+counter = 0
 for node in content.findAll(("h3", {"class": "headline"})):
-    print(node.text)
+    if counter > 10:
+        break
+    print("- ", node.text)
+
+    counter = counter + 1
