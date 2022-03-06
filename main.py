bc80819a3c0ebdbb9c8286762a9827139c224079
anmol <anmol@dellpc.ht.home>
2022-03-06 12:55:06 -0500

    now shows numbers

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ response = requests.get("https://www.cbc.ca/news")
 content = BeautifulSoup(response.content, "html.parser")
 
 node = content.findAll(("h3", {"class": "headline"}))
-for i in range(9):
-    print("- ", node[i+1].text)
+for i in range(10):
+    print(i+1, node[i+1].text)
